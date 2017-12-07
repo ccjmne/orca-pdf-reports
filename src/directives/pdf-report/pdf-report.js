72e1483eb971b145d2cf741a7fcf449ef7952234
ccjmne <ccjmne@gmail.com>
2017-12-07 15:49:07 +0000

    Use $timeout to fix bug where angular wouldn't pick up updates on exposed pdf-contents

--- src/directives/pdf-report/pdf-report.js
+++ src/directives/pdf-report/pdf-report.js
@@ -9,22 +9,22 @@ module.exports = {
     bookmark: '?pdfBookmark',
     footer: '?pdfFooter'
   },
-  controller: ['$transclude', '$element', '$attrs', '$parse', 'pdfReport', function ($transclude, $element, $attrs, $parse, pdfReport) {
+  controller: ['$transclude', '$element', '$attrs', '$parse', '$timeout', 'pdfReport', function ($transclude, $element, $attrs, $parse, $timeout, pdfReport) {
     this.userStyle = pdfReport.userStyle;
     this.hasBookmark = $transclude.isSlotFilled('bookmark');
     $element[0].style.all = 'unset';
 
-    // Expose the HTML contents through the scope variable defined via pdfContents
+    // Expose the element's HTML contents through the scope variable defined via its 'pdf-contents' attribute
     if ($attrs.pdfContents) {
-      const notify = ((assign) => assign.bind(assign, $element.scope()))($parse($attrs.pdfContents).assign);
+      const notify = html => $timeout($parse($attrs.pdfContents).assign.bind(null, $element.scope(), html), 0);
       $element.scope().$watch(() => $element[0].outerHTML, html => notify(`<!DOCTYPE html>
-          <html>
-              <head>
-                  <meta charset='utf-8'>
-                  <style>${require('../../styles/print-only.scss')}</style>
-              </head>
-              <body>${html}</body>
-          </html>`));
+        <html>
+            <head>
+                <meta charset='utf-8'>
+                <style>${require('../../styles/print-only.scss')}</style>
+            </head>
+            <body>${html}</body>
+        </html>`));
     }
   }]
 };
