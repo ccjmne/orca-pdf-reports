f582da7610d4c342c726115a74652e623efc3dfe
ccjmne <ccjmne@gmail.com>
2017-12-10 23:28:22 +0100

    Introduced contenteditable-x component that leverages angular-content-editable and decorates it with moustache-like interpolation

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ require('style-loader!./styles/no-print.scss');
 const angular = require('angular');
 const _ = require('lodash');
 
-module.exports = angular.module('pdf-reports', [require('./directives/directives')])
+require('angular-content-editable');
+
+module.exports = angular.module('pdf-reports', [require('./directives/directives'), 'angular-content-editable'])
+  .directive('contenteditableX', ['$compile', '$parse', '$timeout', function ($compile, $parse, $timeout) {
+    return {
+      transclude: true,
+      replace: true,
+      template: (elem, attrs) => `
+      <span
+          content-editable single-line="true" focus-select="false"
+          ng-model="${attrs.model ||`__contenteditablex_${Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)}`}"
+          ng-transclude></span>`,
+      link: function (scope, element, attrs) {
+        scope.$watch(() => `${$parse(attrs.ngModel)(scope)}:${element[0] === document.activeElement}`, () => $timeout(() => {
+          if (element[0] === document.activeElement) {
+            // decompile <span ng-bind="expr"></span> into {{expr}} notation
+            element.html(element[0].innerHTML.replace(/<span\s+ng-bind="([^"]+)"(?:\s+[\w-]+(?:="[^"]+")?)*>.*?<\/span>/g, '{{$1}}'));
+          } else {
+            // compile {{expr}} notation to <span ng-bind="expr"></span>
+            element.html(element[0].innerHTML.replace(/\{\{([^}]+)\}\}/g, (match, model) => `<span ng-bind="${model.toLowerCase()}"></span>`));
+            $compile(element[0].querySelectorAll('span'))((attrs.scope ? Object.assign(scope.$new(true), $parse(attrs.scope)(scope)) : scope));
+          }
+        }, 0));
+      }
+    };
+  }])
   .directive('getPdf', ['$http', function ($http) {
     const apiSvc = {
       post: function (url, data, config) {
