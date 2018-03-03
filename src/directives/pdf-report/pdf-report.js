0e2ec154ff6bf9f8a8d9d17549cb372839cecb45
ccjmne <ccjmne@gmail.com>
2018-03-03 17:53:42 +0100

    Tailor pdf-report's exposed html contents for use with wkhtmltopdf

--- src/directives/pdf-report/pdf-report.js
+++ src/directives/pdf-report/pdf-report.js
@@ -10,36 +10,69 @@ module.exports = {
     bookmark: '?pdfBookmark',
     footer: '?pdfFooter'
   },
+  bindings: {
+    format: '=',
+    pdfContents: '=',
+    clone: '=',
+    hook: '&'
+  },
   controller: ['$transclude', '$element', '$attrs', '$parse', '$timeout', 'pdfReport', function ($transclude, $element, $attrs, $parse, $timeout, pdfReport) {
     this.userStyle = pdfReport.userStyle;
     this.hasBookmark = $transclude.isSlotFilled('bookmark');
     $element[0].style.all = 'unset';
 
-    // Expose the element's HTML contents through the scope variable defined via its 'pdf-contents' attribute
-    if ($attrs.pdfContents || $attrs.clone) {
-      const notifyHtml = $attrs.pdfContents && (html => $timeout($parse($attrs.pdfContents).assign.bind(null, $element.scope(), html), 0));
-      const notifyNode = $attrs.clone && (node => $timeout($parse($attrs.clone).assign.bind(null, $element.scope(), node), 0));
-      $element.scope().$watch(() => $element[0].outerHTML, () => {
+    this.$onInit = () => {
+      if (!this.hook) {
+        return;
+      }
+
+      if (this.format instanceof Array) {
+        $element.scope().$watchCollection(() => this.format, (classes, previous) => {
+          $element[0].classList.remove(...previous);
+          $element[0].classList.add(...classes);
+          this.notifyHook();
+        });
+      }
+
+      $element.scope().$watch(() => $element[0].outerHTML, this.notifyHook);
+    };
+
+    this.notifyHook = () => {
+      $timeout(() => {
         const node = $element[0].cloneNode(true);
         node.setAttribute('preview', true);
-        if (notifyNode) { notifyNode(node); }
-        if (notifyHtml) { notifyHtml(`<!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset='utf-8'>
-                <style>
-                    body,
-                    html {
-                      height: 100%;
-                      margin: 0;
-                      font-size: 1.1em;
-                    }
-                    ${require('../../styles/print-only.scss')}
-                </style>
-            </head>
-            <body>${node.outerHTML}</body>
-        </html>`); }
+        if (this.hook) {
+          this.hook({
+            $clone: node,
+            $contents: `<!DOCTYPE html>
+              <html>
+                  <head>
+                      <meta charset='utf-8'>
+                      <style>
+                          body,
+                          html {
+                            height: 100%;
+                            margin: 0;
+                          }
+
+                          pdf-report {
+                            max-height: 100% !important;
+                            max-width: 100% !important;
+                          }
+
+                          pdf-report .pdf-root {
+                            max-height: 100% !important;
+                            max-width: 100% !important;
+                            zoom: ${1 / 0.78125};
+                          }
+                          ${require('../../styles/print-only.scss')}
+                      </style>
+                  </head>
+                  <body>${node.outerHTML}</body>
+              </html>`
+          });
+        }
       });
-    }
+    };
   }]
 };
